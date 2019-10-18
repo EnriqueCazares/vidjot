05f14ec02a70cda615a7bdbe90932bdd9a86a48f
Enrique Cazares <cazaresenrique97@gmail.com>
2019-10-18 00:33:36 -0500

    DropDown Menu and Form for adding video ideas

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev') // Posiblemente agregar como segundo parámetro los warning que arroja al conectar con mongo
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -29,6 +42,11 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
     res.render('about');
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
 });
 
 const port = 5000;
